e5904e058515496ed798d7931e32a508e66389d8
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-06-03 14:59:42 +0500

    fix: remove unused import

--- src/utils/prompt-calculator.ts
+++ src/utils/prompt-calculator.ts
@@ -1,4 +1,4 @@
-import { encoding_for_model, get_encoding } from 'tiktoken';
+import { encoding_for_model } from 'tiktoken';
 
 export const getTokenCount = (input: string) => {
   const encoding = encoding_for_model('gpt-4-turbo');
